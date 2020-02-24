7982e3034cfc8add9f62a6f4276fa262b2630cb3
orbtl <twinsendude@gmail.com>
2020-02-24 14:26:58 -0800

    Updated Reservation model to not have required fields to allow more flexibility and added Notes string

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -9,26 +9,23 @@ namespace massage.Models
         [Key]
         public int ReservationId { get; set; }
 
-        [Required]
+        public string Notes { get; set; }
+
         public int CustomerId { get; set; }
         public Customer Customer { get; set; }
 
-        [Required]
         public int PractitionerId { get; set; }
         public User Practitioner { get; set; }
 
         public int CreatorId { get; set; }
         public User Creator { get; set; }
 
-        [Required]
         public int ServiceId { get; set; }
         public Service Service { get; set; }
 
-        [Required]
         public int RoomId { get; set; }
         public Room Room { get; set; }
 
-        [Required]
         public int TimeslotId { get; set; }
         public Timeslot Timeslot { get; set; }
 
