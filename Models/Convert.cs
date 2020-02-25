4e29e07b241af6b9bca902a92695b0224c741441
orbtl <twinsendude@gmail.com>
2020-02-25 13:15:31 -0800

    completed first draft of convert method back to query ready list from front end dictionaries

--- Models/Convert.cs
+++ Models/Convert.cs
@@ -55,5 +55,36 @@ namespace massage.Models
             }
             return formattedSchedule;
         }
+        public static List<PSchedule> ScheduleToQuery(Dictionary<string, Dictionary<string, bool>> frontEndPS, int practID)
+        {
+            List<PSchedule> queryReadyList = new List<PSchedule>();
+            foreach (KeyValuePair<string, Dictionary<string, bool>> outerKVP in frontEndPS)
+            {
+                PSchedule thisPS = new PSchedule();
+                thisPS.DayOfWeek = outerKVP.Key;
+                thisPS.PractitionerId = practID;
+                thisPS.Approved = false;
+                foreach (KeyValuePair<string, bool> innerKVP in outerKVP.Value)
+                {
+                    string unParsedHour = innerKVP.Key.Substring(0,4);
+                    string parsedHour = "";
+                    int numHour = Int32.Parse(unParsedHour[0].ToString());
+                    if (unParsedHour[0] == '1')
+                    {
+                        parsedHour = unParsedHour.Substring(0,2);
+                    }
+                    else if (unParsedHour[1] == 'p') // like 3pm, numhour is 3, needs to add 12 so it's 15
+                    {
+                        parsedHour = (numHour + 12).ToString();
+                    }
+                    else { // am, like 9am, numhour is 9 so just set it to parsedHour
+                        parsedHour = numHour.ToString();
+                    }
+                    thisPS.GetType().GetProperty("t" + parsedHour).SetValue(thisPS, innerKVP.Value);
+                }
+                queryReadyList.Add(thisPS);
+            }
+            return queryReadyList;
+        }
     }
 }
