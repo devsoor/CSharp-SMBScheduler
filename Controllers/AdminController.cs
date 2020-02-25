49c742830af0c915851bdc59427facddeada7a04
gmldbs95 <2man.kaelo@gmail.com>
2020-02-25 14:47:50 -0800

    admin & prac controller initial

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -14,9 +14,10 @@ using massage.Models;
 namespace massage.Controllers
 {
     [Authorize]
+    [Route("admin")]
     public class AdminController : Controller
     {
-        // database setup
+        // Database setup
         public ProjectContext dbContext;
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
@@ -30,5 +31,115 @@ namespace massage.Controllers
             _signInManager = signInManager;
         }
 
-    }
-}
+//////////////////////////////// GET ////////////////////////////////
+
+        // Admin: dashboard
+        [HttpGet("dashboard")]
+        public IActionResult Dash(){
+            // stuff
+            return View("Index");
+        }
+
+        // EMPLOYEE
+        // Admin: Employee Profile FORM
+        [HttpGet("user_profile/{userId}")]
+        public IActionResult UserProfile(int userId){
+            // stuff
+            return View();
+        }
+
+        // Admin: Employee template FORM
+        [HttpGet("user_template/{userId}")]
+        public IActionResult UserTemplate(int userId){
+            // stuff
+            return View();
+        }
+
+        // Admin: Employee Current Schedule
+        [HttpGet("user_current_sched/{userId}")]
+        public IActionResult UserCurrentSched(int userId){
+            // stuff
+            return View();
+        }
+
+        // INSURANCE
+        // Admin: View list of all insurance LIST
+        [HttpGet("insurances")]
+        public IActionResult AllInsurance(){
+            // stuff
+            return View();
+        }
+
+        // Admin: Add new insurance FORM
+        [HttpGet("new_insurance")]
+        public IActionResult NewInsurance(){
+            // stuff
+            return View();
+        }
+
+        // Admin: Edit insurance FORM
+        [HttpGet("edit_insurance/{insurId}")]
+        public IActionResult EditInsurance(){
+            // stuff
+            return View();
+        }
+
+        // Admin: DELETE
+        [HttpGet("del_insurance/{insurId}")]  // this can be post of u REALLY want it to be.....
+        public IActionResult DeleteInsurance(){
+            return View();
+        }
+
+//////////////////////////////// POST ////////////////////////////////
+        
+        // Admin: Employee Profile FORM-SUBMIT
+        [HttpPost]
+        public IActionResult UserProfileSubmit(){
+            if(ModelState.IsValid){
+                // stuff
+                return RedirectToAction("UserProfile");
+            }
+            else {
+                return View("UserProfile");
+
+            }
+        }
+
+        // Admin: Employee template FORM-SUBMIT
+        [HttpPost]
+        public IActionResult UserTemplateSubmit(){
+            if(ModelState.IsValid){
+                // stuff
+                return RedirectToAction("UserProfile");
+            }
+            else {
+                return View("UserProfile");
+            }
+        }
+
+        // Admin: Add new insurance FORM-SUBMIT
+        [HttpPost]
+        public IActionResult CreateInsuranceSubmit(){
+            if(ModelState.IsValid){
+                // stuff
+                return RedirectToAction("AllInsurance");
+            }
+            else {
+                return View("UserProfile");
+            }
+        }
+
+        // Admin: Updating insurance FORM-SUBMIT
+        [HttpPost]
+        public IActionResult UpdateInsuranceSubmit(){
+            if(ModelState.IsValid){
+                // stuff
+                return RedirectToAction("AllInsurance");
+            }
+            else {
+                return View("UserProfile");
+            }
+        }
+
+    }   // END CONTROLLER
+}   // END ALL
--- Controllers/PractitionerController.cs
+++ Controllers/PractitionerController.cs
@@ -14,9 +14,10 @@ using massage.Models;
 namespace massage.Controllers
 {
     [Authorize]
+    [Route("practitioner")]
     public class PractitionerController : Controller
     {
-        // database setup
+        // Database setup
         public ProjectContext dbContext;
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
@@ -30,5 +31,67 @@ namespace massage.Controllers
             _signInManager = signInManager;
         }
 
-    }
-}
+//////////////////////////////// GET ////////////////////////////////
+
+        // Practitioner dashboard
+        [HttpGet("dashboard")]
+        public IActionResult Dash(){
+            // stuff
+            return View("Index");
+        }
+
+        // Practitioner Schedule View w/ current
+        [HttpGet("schedule")]
+        public IActionResult PracSched(){
+            // stuff
+            return View();
+        }
+
+        // Practitioner Template View FORM
+        [HttpGet("template")]
+        public IActionResult PracTemplate(){
+            // stuff
+            return View();
+        }
+
+        // Practitioner Profile View
+        [HttpGet("profile")]
+        public IActionResult PracProfile(){
+            // stuff
+            return View();
+        }
+
+        // Practitioner Update Profile FORM
+        [HttpGet("update_profile")]
+        public IActionResult UpdatePracProf(){
+            // stuff
+            return View();
+        }
+
+//////////////////////////////// POST ////////////////////////////////
+
+        // Practitioner: template FORM ON-SUBMIT
+        [HttpPost]
+        public IActionResult PracTemplateSubmit(){
+            if(ModelState.IsValid){
+                // stuff
+                return RedirectToAction("Dash");
+            }
+            else {
+                return View("UserProfile");
+            }
+        }
+
+        // Practitioner: profile FORM ON-SUBMIT
+        [HttpPost]
+        public IActionResult PracProfileSubmit(){
+            if(ModelState.IsValid){
+                // stuff
+                return RedirectToAction("PracProfile");
+            }
+            else {
+                return View("UserProfile");
+            }
+        }
+    }   // END CONTROLLER
+}   // END ALL
